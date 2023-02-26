2843a89830a21a4c4146e3b913582d7be5355470
dmrozik87 <dmrozik87@gmail.com>
2023-02-26 17:16:32 +0100

    App working, ability to choose how many numbers to toggle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [numbers, setNumbers] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [howManyNumbers, setHowManyNumbers] = useState(0);
+
+  function createRandomNumbersArray() {
+    const randomNumbersArray = [];
+    while (randomNumbersArray.length < howManyNumbers) {
+      let newNumber = Math.floor(Math.random() * howManyNumbers) + 1;
+      if (!randomNumbersArray.includes(newNumber)) {
+        randomNumbersArray.push(newNumber);
+      }
+    }
+    return randomNumbersArray;
+  }
+
+  function handleClick() {
+    setNumbers([]);
+    setIndex(0);
+    let counter = 0;
+    setNumbers(createRandomNumbersArray());
+    let id = setInterval(() => {
+      setIndex((prevIndex) => prevIndex + 1);
+      counter++;
+      if (counter > howManyNumbers) clearInterval(id);
+    }, 500);
+  }
+
+  function handleChange(event) {
+    setHowManyNumbers(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputField">
+        <label htmlFor="howManyNumbers">Ile liczb ma Twoje Bingo?</label>
+        <input type="number" name="howManyNumbers" onChange={handleChange}></input>
+      </div>
+      <button onClick={handleClick}>Start</button>
+      <div className="display">
+        <h1>{numbers[index]}</h1>
+      </div>
     </div>
   );
 }
